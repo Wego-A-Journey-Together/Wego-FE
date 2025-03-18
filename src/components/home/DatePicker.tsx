90a7867c110770ef41958011d862be1cf1d31c0e
incolore9 <hansol.woff@gmail.com>
2025-03-18 11:00:58 +0900

    design: 날짜 필터링 디자인 수정

---
+++ src/components/home/DatePicker.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from '@/components/ui/popover';
+import { cn } from '@/lib/utils';
+import { format } from 'date-fns';
+import * as React from 'react';
+
+export default function DatePicker() {
+    const [date, setDate] = React.useState<Date>();
+
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button
+                    variant={'outline'}
+                    className={cn(
+                        'w-[280px] justify-start text-left font-normal pl-7 pr-[18px] py-4 h-auto',
+                        !date && 'text-muted-foreground',
+                    )}
+                >
+                    <img
+                        src="/icon/home/calenderIcon.svg"
+                        alt="달력 아이콘"
+                        className="w-[17px] mr-7"
+                    />
+
+                    {date ? (
+                        format(date, 'PPP')
+                    ) : (
+                        <span>날짜는 언제인가요?</span>
+                    )}
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0">
+                <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={setDate}
+                    initialFocus
+                />
+            </PopoverContent>
+        </Popover>
+    );
+}
