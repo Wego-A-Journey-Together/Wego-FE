aa20aa465ea45534a6b5a31f8cc9d6342c0e9ce0
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2025-03-18 11:20:54 +0900

    design: 캐러셀 패딩 조정, 유저 프로필 호버효과 추가 (#33)

--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -1,7 +1,7 @@
 import MultiLingualToggler from '@/components/MultiLingual/MultiLingualToggler';
 import Logo from '@/components/Nav/Logo';
-import { SearchBar } from '@/components/Nav/SearchBar';
-import { Button } from '@/components/ui/button';
+import {SearchBar} from '@/components/Nav/SearchBar';
+import {Button} from '@/components/ui/button';
 import Link from 'next/link';
 
 export default function NavBar() {
@@ -10,21 +10,21 @@ export default function NavBar() {
             {/*좌측 섹션*/}
             <section className="flex items-center gap-3">
                 {/*로고*/}
-                <Logo />
+                <Logo/>
                 {/*서치바*/}
-                <SearchBar className="bg-neutral-100 dark:bg-neutral-800" />
+                <SearchBar className="bg-neutral-100 dark:bg-neutral-800"/>
             </section>
 
             {/*우측섹션*/}
-            <section className="flex items-center">
+            <section className=" items-center hidden md:flex">
                 <div className={`font-semibold text-gray-600`}>동행찾기</div>
-                <div className={`w-[30px]`} />
+                <div className={`w-[30px]`}/>
                 <Button className={`px-7 py-2.5`}>
                     <Link href={`/auth/login`}>로그인 및 회원가입</Link>
                 </Button>
-                <div className={`w-[20px]`} />
+                <div className={`w-[20px]`}/>
 
-                <MultiLingualToggler />
+                <MultiLingualToggler/>
             </section>
         </header>
     );
