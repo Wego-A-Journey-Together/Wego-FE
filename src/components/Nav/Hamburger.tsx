c884cd9ded5a789cbc9d0c25902ebfa49ba4ccf2
jungdadak <junghyun21110@gmail.com>
2025-03-20 13:39:26 +0900

    feat: 검색바 외부 클릭, 키보드 입력시 입력 취소

---
+++ src/components/Nav/Hamburger.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import LoginBtn from '@/components/Btn/LoginBtn';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { cn } from '@/lib';
+import { Menu, X } from 'lucide-react';
+import { useRef, useState } from 'react';
+
+export default function Hamburger() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const menuRef = useRef(null);
+
+    const handleMenuToggle = () => {
+        setIsMenuOpen((prev) => !prev);
+    };
+
+    return (
+        <DropdownMenu open={isMenuOpen} onOpenChange={setIsMenuOpen}>
+            <DropdownMenuTrigger asChild>
+                <div
+                    className="relative cursor-pointer"
+                    ref={menuRef}
+                    onClick={handleMenuToggle}
+                >
+                    <Menu
+                        className={cn(
+                            'h-5 w-5 transition-all duration-300 ease-in-out sm:h-7.5 sm:w-7.5',
+                            isMenuOpen
+                                ? 'scale-0 rotate-90 opacity-0'
+                                : 'scale-100 rotate-0 opacity-100',
+                        )}
+                    />
+                    <X
+                        className={cn(
+                            'absolute top-0 left-0 h-5 w-5 transition-all duration-300 ease-in-out sm:h-7.5 sm:w-7.5',
+                            isMenuOpen
+                                ? 'scale-100 rotate-0 opacity-100'
+                                : 'scale-0 rotate-90 opacity-0',
+                        )}
+                    />
+                </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="center" className="w-auto">
+                <DropdownMenuItem asChild>
+                    <LoginBtn />
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -1,28 +1,19 @@
 'use client';
 
+import LoginBtn from '@/components/Btn/LoginBtn';
 import MultiLingualToggler from '@/components/MultiLingual/MultiLingualToggler';
+import Hamburger from '@/components/Nav/Hamburger';
 import Logo from '@/components/Nav/Logo';
 import { SearchBar } from '@/components/Nav/SearchBar';
-import { Button } from '@/components/ui/button';
 import useMediaQuery from '@/hooks/useMediaQuery';
-import { Menu } from 'lucide-react';
-import Link from 'next/link';
-import { useState } from 'react';
 
 export default function NavBar() {
-    const isMobile = useMediaQuery('(max-width: 639px)');
-    const isTablet = useMediaQuery('(min-width: 640px) and (max-width: 767px)');
     const isDesktop = useMediaQuery('(min-width: 768px)');
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
         <header className="flex h-18 w-full items-center justify-between">
             {/*햄버거 메뉴*/}
-            {!isDesktop && (
-                <section>
-                    <Menu className="h-5 w-5 sm:h-7.5 sm:w-7.5" />
-                </section>
-            )}
+            {!isDesktop && <Hamburger />}
 
             {/*좌측 섹션*/}
             <section className="absolute left-1/2 -translate-x-1/2 transform items-center gap-3 md:static md:flex md:translate-x-0">
@@ -41,9 +32,7 @@ export default function NavBar() {
                 )}
                 {isDesktop && (
                     <>
-                        <Button className={`px-7 py-2.5 md:flex`}>
-                            <Link href={`/auth/login`}>로그인 및 회원가입</Link>
-                        </Button>
+                        <LoginBtn />
 
                         <div className={`w-[20px]`} />
                     </>
