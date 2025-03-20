66f92e6e0d9935b8b5b478f0d161daae787345f3
jungdadak <junghyun21110@gmail.com>
2025-03-20 10:26:04 +0900

    design: 다국어 반응형

--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -1,30 +1,34 @@
 import MultiLingualToggler from '@/components/MultiLingual/MultiLingualToggler';
 import Logo from '@/components/Nav/Logo';
-import {SearchBar} from '@/components/Nav/SearchBar';
-import {Button} from '@/components/ui/button';
+import { SearchBar } from '@/components/Nav/SearchBar';
+import { Button } from '@/components/ui/button';
+import { Menu } from 'lucide-react';
 import Link from 'next/link';
 
 export default function NavBar() {
     return (
-        <header className="h-18 flex items-center justify-between w-full">
+        <header className="flex h-18 w-full items-center justify-between">
+            {/*모바일 햄버거 메뉴*/}
+            <section className="md:hidden">
+                <Menu />
+            </section>
+
             {/*좌측 섹션*/}
             <section className="flex items-center gap-3">
                 {/*로고*/}
-                <Logo/>
+                <Logo />
                 {/*서치바*/}
-                <SearchBar className="bg-neutral-100 dark:bg-neutral-800"/>
+                <SearchBar className="bg-neutral-100 dark:bg-neutral-800" />
             </section>
 
             {/*우측섹션*/}
-            <section className=" items-center hidden md:flex">
-                <div className={`font-semibold text-gray-600`}>동행찾기</div>
-                <div className={`w-[30px]`}/>
-                <Button className={`px-7 py-2.5`}>
+            <section className="items-center md:flex">
+                <Button className={`hidden px-7 py-2.5 md:flex`}>
                     <Link href={`/auth/login`}>로그인 및 회원가입</Link>
                 </Button>
-                <div className={`w-[20px]`}/>
+                <div className={`w-[20px]`} />
 
-                <MultiLingualToggler/>
+                <MultiLingualToggler />
             </section>
         </header>
     );
