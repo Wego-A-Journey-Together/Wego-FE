7244dd3d8ea9626bc6312397ac1e82a9579d08b6
jungdadak <junghyun21110@gmail.com>
2025-03-21 16:11:49 +0900

    chore: commit for rebase

---
+++ src/app/[lang]/detail/[id]/types.ts
@@ -0,0 +1,15 @@
+export interface PostDeatil {
+    id: number;
+    title: string;
+    imageSrc: string;
+    profileImage: string;
+    userName: string;
+    age: string;
+    gender: string;
+    startDate: string;
+    endDate: string;
+    userId: string;
+    view?: number;
+    bookMark?: boolean;
+    commentCount?: number;
+}
