c2628d312d392fb2f05b6bdf03c3c621f48b275b
jungdadak <junghyun21110@gmail.com>
2025-03-21 17:40:51 +0900

    탭 하단 디자인을 보더에서 박스형으로 변경 및 레이아웃 시프트 수정

--- src/components/detail/TabSection.tsx
+++ src/components/detail/TabSection.tsx
@@ -3,11 +3,10 @@ import Tab from '@/components/detail/Tab';
 export default function TabSection() {
     return (
         // 탭 섹션
-        <section
-            className={`sticky top-18 mt-17.5 border-b border-b-[#AAAAAAA]`}
-        >
+        <section className={`sticky top-18 mt-17.5`}>
             <p className={`sr-only`}>탭그룹</p>
             <Tab />
+            <div className={`h-px w-full rounded-full bg-[#D9D9D9]`} />
         </section>
     );
 }
