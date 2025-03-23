a217e04e204cafbaadaf953537e676100576ca13
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2025-03-23 18:50:49 +0900

    feat: 탭 클릭시 인터섹션 옵저버 비활성화 로직 추가

--- src/components/detail/Tab.tsx
+++ src/components/detail/Tab.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { cn } from '@/lib';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const tabItems = [
     '상세 정보',
@@ -20,11 +20,16 @@ export default function Tab({
 }) {
     const [selectedTab, setSelectedTab] = useState<TabItem>(tabItems[0]);
 
+    // 탭을 눌러 스크롤시에 Observer 함께 동작하여 UI 불안정한 문제 해결을 위한 플래그
+    const isScrollByTabClick = useRef<boolean>(false);
+
     // IntersectionObserver 콜백함수
-    const handleIntersection: IntersectionObserverCallback = (
-        entries,
-        observer,
-    ) => {
+    const handleIntersection: IntersectionObserverCallback = (entries) => {
+        /**
+         * 탭 클릭으로 인한 scrollIntoView 함수 실행중엔 얼리리턴해서 무시함
+         */
+        if (isScrollByTabClick.current) return;
+
         /**
          * 각 entry가 감지되었을 때,
          * 해당 entry.target과 일치하는 ref를 찾아 탭 인덱스를 계산하고,
@@ -66,6 +71,12 @@ export default function Tab({
             behavior: 'smooth',
             block: 'start',
         });
+        isScrollByTabClick.current = true;
+
+        // 잠시 후 Ref 값을 false 로 변경
+        setTimeout(() => {
+            isScrollByTabClick.current = false;
+        }, 500);
     };
     return (
         <div
