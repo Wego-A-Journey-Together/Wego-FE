5e0da1d148db25b7dc724503f6d0617f809af7c6
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2025-03-23 15:35:56 +0900

    chore: useRef setting

--- src/components/detail/TabSection.tsx
+++ src/components/detail/TabSection.tsx
@@ -1,37 +1,53 @@
+'use client';
+
 import PostComment from '@/components/detail/PostComment';
 import PostContent from '@/components/detail/PostContent';
 import PostInput from '@/components/detail/PostInput';
 import PostLocation from '@/components/detail/PostLocation';
 import PostReview from '@/components/detail/PostReview';
 import Tab from '@/components/detail/Tab';
+import { useRef } from 'react';
 
 import { TrendingPost } from '../../../public/data/trending';
 
 export default function TabSection({ post }: { post: TrendingPost }) {
+    const contentAreaRef = useRef<HTMLDivElement>(null);
+    const locationAreaRef = useRef<HTMLDivElement>(null);
+    const memberAreaRef = useRef<HTMLDivElement>(null);
+    const commentAreaRef = useRef<HTMLDivElement>(null);
+    const reviewAreaRef = useRef<HTMLDivElement>(null);
     return (
         <>
-            // 탭 섹션
             <section
                 className={`sticky top-18 z-50 mt-17.5 border-b border-b-[#AAAAAAA]`}
             >
                 <p className={`sr-only`}>탭그룹</p>
                 <Tab />
             </section>
-            <section>
+            {/*상세 정보 섹션*/}
+            <section ref={contentAreaRef}>
                 <PostContent post={post} />
             </section>
             {/*지도 섹션*/}
-            <section>
+            <section ref={locationAreaRef}>
                 <PostLocation location={post.location} />
             </section>
             {/*todo: 멤버 소개 섹션*/}
+            <section ref={memberAreaRef}>
+                <div
+                    className={`bg-sky-blue mt-12.5 flex h-40 items-center justify-center text-2xl font-black text-white`}
+                >
+                    <p> 멤버 소개 섹션이에</p>
+                    <p className={`animate-spin`}> 요</p>
+                </div>
+            </section>
             {/*댓글 섹션*/}
-            <section className={`mt-15`}>
+            <section ref={commentAreaRef} className={`mt-15`}>
                 <PostComment />
                 <PostInput />
             </section>
             {/*리뷰 섹션*/}
-            <section>
+            <section ref={reviewAreaRef}>
                 <PostReview />
             </section>
         </>
