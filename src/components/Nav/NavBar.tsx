76e2fe8e8bc50543ba52f91dcbcdec961ce819c4
incolore9 <hansol.woff@gmail.com>
2025-03-25 11:42:16 +0900

    fix: 헤더 반응형을 미디어 쿼리가 아닌 css로 처리

--- src/components/Nav/NavBar.tsx
+++ src/components/Nav/NavBar.tsx
@@ -5,7 +5,6 @@ import MultiLingualToggler from '@/components/MultiLingual/MultiLingualToggler';
 import Hamburger from '@/components/Nav/Hamburger';
 import Logo from '@/components/Nav/Logo';
 import { SearchBar } from '@/components/Nav/SearchBar';
-import useMediaQuery from '@/hooks/useMediaQuery';
 import { cn } from '@/lib';
 
 interface NavProps {
@@ -13,8 +12,6 @@ interface NavProps {
 }
 
 export default function NavBar({ className }: NavProps) {
-    const isDesktop = useMediaQuery('(min-width: 768px)');
-
     return (
         <header
             className={cn(
@@ -22,34 +19,33 @@ export default function NavBar({ className }: NavProps) {
                 className,
             )}
         >
-            {/*햄버거 메뉴*/}
-            {!isDesktop && <Hamburger />}
+            {/* 데스크탑 레이아웃 */}
+            <div className="hidden w-full md:flex md:items-center md:justify-between">
+                <section className="flex items-center gap-3">
+                    <Logo />
+                    <SearchBar className="bg-neutral-100 dark:bg-neutral-800" />
+                </section>
 
-            {/*좌측 섹션*/}
-            <section className="absolute left-1/2 -translate-x-1/2 transform items-center gap-3 md:static md:flex md:translate-x-0">
-                {/*로고*/}
-                <Logo />
-                {/*서치바*/}
-                {isDesktop && (
-                    <SearchBar className="bg-neutral-100 md:flex dark:bg-neutral-800" />
-                )}
-            </section>
+                <section className="flex items-center">
+                    <LoginBtn />
+                    <div className="w-[20px]" />
+                    <MultiLingualToggler />
+                </section>
+            </div>
 
-            {/*우측섹션*/}
-            <section className="flex items-center">
-                {!isDesktop && (
-                    <SearchBar className="bg-neutral-100 md:flex dark:bg-neutral-800" />
-                )}
-                {isDesktop && (
-                    <>
-                        <LoginBtn />
+            {/* 모바일 레이아웃 */}
+            <div className="flex w-full items-center justify-between md:hidden">
+                <Hamburger />
 
-                        <div className={`w-[20px]`} />
-                    </>
-                )}
+                <section className="absolute left-1/2 -translate-x-1/2 transform">
+                    <Logo />
+                </section>
 
-                <MultiLingualToggler />
-            </section>
+                <section className="flex items-center">
+                    <SearchBar className="bg-neutral-100 dark:bg-neutral-800" />
+                    <MultiLingualToggler />
+                </section>
+            </div>
         </header>
     );
 }
