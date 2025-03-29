cec01dd1531662cb5b62f69915f8ab492e15ccc3
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2025-03-29 14:20:41 +0900

    chore: rebase

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from 'next';
 
-
-
-
-
 const nextConfig: NextConfig = {
     reactStrictMode: true, // 개발 중 잠재적 문제를 조기에 발견하기 위한 설정
     // Next.js 에서 이제 디폴트로 관리되어 불필요한 설정이라 주석 처리 하였습니다.
@@ -11,7 +7,14 @@ const nextConfig: NextConfig = {
     compress: true, // 응답 압축으로 성능 향상
     poweredByHeader: false, // 보안을 위해 X-Powered-By 헤더 제거
     images: {
-        remotePatterns: [], // 외부 이미지 도메인 허용 목록 -> domains -> remotePatterns 로 수정 하였습니다.
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '8080', // 포트도 필요하다면 추가합니다. 예: 8080 포트
+                pathname: '/static/**', // 경로 패턴
+            },
+        ], // 외부 이미지 도메인 허용 목록 -> domains -> remotePatterns 로 수정 하였습니다.
         formats: ['image/webp', 'image/avif'], // 최신 이미지 포맷 지원
     },
     // 미들웨어에서 이미 처리하고 있고, 레거시 설정이라 주석처리 하였습니다.
