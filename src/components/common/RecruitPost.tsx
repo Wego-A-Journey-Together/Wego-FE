eed6fec8e773c4c0b753c2a94efa083d7abaa873
incolore9 <hansol.woff@gmail.com>
2025-04-01 10:59:42 +0900

    design: 홈 리스트 컴포넌트 썸네일 이미지 추가

--- src/components/common/RecruitPost.tsx
+++ src/components/common/RecruitPost.tsx
@@ -4,30 +4,47 @@ import { PostContentProps } from '@/types/PostContent';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { Badge } from '../ui/badge';
+
 //todo : 현재는 낙관적 업데이트를 위한 단순한 id 만 정의 (데이터 바인딩 x )
 
 export default function RecruitPost({ post }: PostContentProps) {
     return (
         <article className="flex flex-col gap-2.5 rounded-xl bg-[#f5f6f7] px-[34px] py-[30px]">
             {/* 헤더 섹션 */}
-            <header className="flex w-full flex-col">
+            <div className="flex w-full flex-col">
                 <Link href={`/detail/${post.id}`}>
-                    <div className="mb-2 flex w-full items-center gap-2">
-                        <span
-                            className={`${post.isGroupOpen ? 'bg-[#fa9b56]' : 'bg-[#999999]'} rounded-[20px] px-3 py-1 text-xs font-semibold text-white`}
-                        >
-                            {post.isGroupOpen ? '동행 구함' : '모집 마감'}
-                        </span>
+                    <div className="relative">
+                        <div className="mb-1.5 flex w-full items-center gap-2">
+                            <Badge
+                                variant={
+                                    post.isGroupOpen ? 'default' : 'disable'
+                                }
+                            >
+                                {post.isGroupOpen ? '동행 구함' : '모집 마감'}
+                            </Badge>
 
-                        {/* 글제목  */}
-                        <h1 className="flex-1 truncate text-xl font-bold text-black">
-                            {post.title}
-                        </h1>
+                            {/* 글제목  */}
+                            <h1 className="flex-1 truncate text-xl font-bold text-black">
+                                {post.title}
+                            </h1>
+                        </div>
+
+                        {/* 썸네일 이미지 */}
+                        {post.imageSrc && (
+                            <div className="absolute top-0 right-0 h-[98px] w-[98px] overflow-hidden rounded-xl">
+                                <Image
+                                    src={post.imageSrc}
+                                    alt="게시글 썸네일"
+                                    fill
+                                    className="object-cover"
+                                />
+                            </div>
+                        )}
                     </div>
 
                     {/* 모집 정보 */}
-
-                    <div className="mb-3.5 flex items-center gap-2.5">
+                    <div className="mb-3 flex items-center gap-2.5">
                         <span className="text-sm text-[#333333]">
                             {/* 날짜 빼서 며칠 일정인지 계산 로직 필요 */}
                             {`${post.startDate} - ${post.endDate} (${'n'}일)`}
@@ -58,7 +75,7 @@ export default function RecruitPost({ post }: PostContentProps) {
                                 key={index}
                                 className="relative rounded px-1.5 py-1"
                             >
-                                <div className="text-bg-sky-blue z-10 text-[15px] leading-[19.1px] font-medium tracking-[0.47px] whitespace-nowrap">
+                                <div className="text-sky-blue z-10 text-[15px] leading-[19.1px] font-medium tracking-[0.47px] whitespace-nowrap">
                                     {hashtag}
                                 </div>
                                 <div className="bg-sky-blue absolute top-0 left-0 h-[27px] w-full rounded opacity-[0.08]" />
@@ -71,12 +88,12 @@ export default function RecruitPost({ post }: PostContentProps) {
                         {extractPreview(post.content)}
                     </p>
                 </Link>
-            </header>
+            </div>
 
             <hr className="mb-3.5 h-px w-full border-0 bg-gray-200" />
 
             {/* 유저 프로필 */}
-            <section className="flex w-full flex-col items-start justify-between gap-4 md:flex-row md:items-center md:gap-2">
+            <div className="flex w-full flex-col items-start justify-between gap-4 md:flex-row md:items-center md:gap-2">
                 <Link href={`/profile/${post.userId}`}>
                     {/* 아이콘 */}
                     <div className="flex items-center gap-3">
@@ -112,12 +129,11 @@ export default function RecruitPost({ post }: PostContentProps) {
                     </div>
                 </Link>
 
-                {/* 상호작용 버튼 */}
+                {/*낙관적 업데이트 찜 버튼*/}
                 <div className="flex w-full items-center gap-2 md:w-auto">
-                    {/*낙관적 업데이트 찜 버튼*/}
                     <Like id={post.id} className={`px-0`} />
                 </div>
-            </section>
+            </div>
         </article>
     );
 }
