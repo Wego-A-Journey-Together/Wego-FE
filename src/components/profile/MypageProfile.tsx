c651f6f94fab0f873770114c29fed0a476629b14
incolore9 <hansol.woff@gmail.com>
2025-04-01 18:44:33 +0900

    design: 인증 아이콘 추가

--- src/components/profile/MypageProfile.tsx
+++ src/components/profile/MypageProfile.tsx
@@ -80,6 +80,12 @@ export default function MypageProfile({ user }: MypageProfileProps) {
 
                     <div className="flex flex-col gap-2">
                         <div className="flex items-center gap-1">
+                            <Image
+                                width={13}
+                                height={13}
+                                alt="verifiedCheck"
+                                src={'/icon/profile/verifiedCheck.svg'}
+                            />
                             <span className="text-xs text-[#005ECA]">
                                 프로필 작성 완료
                             </span>
