8328d89cc88f741f7361f60bb12b31168714a4d8
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2025-04-03 13:36:13 +0900

    feat: 서버 무응답 방어코드 작성 (#105)

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -70,16 +70,23 @@ export default async function RootLayout({
     //----------------------------------------
 
     //-------------------유저 정보 SSR ----------------------------
+    const controller = new AbortController();
     const NEST_BFF_URL =
         process.env.NEXT_BFF_URL || 'https://gateway.wego-travel.click';
     let user = null;
 
+    // vercel 504 에러를 막기 위해 서버가 내려간 경우 AbortController 타이머 등록
+    const beTimeout = setTimeout(() => {
+        controller.abort();
+    }, 3000);
     try {
         const res = await fetch(`${NEST_BFF_URL}/api/user/me`, {
             headers: {
                 cookie: (await headers()).get('cookie') || '',
             },
             cache: 'no-store',
+            //컨트롤러 등록
+            signal: controller.signal,
         });
 
         if (res.ok) {
@@ -94,6 +101,8 @@ export default async function RootLayout({
         // 서버가 죽었거나 네트워크 문제일 경우 ( fetch 는 !res.ok 를 throw 하지 않는다!! ) -> 로그인상태 x
         console.error('user/me 네트워크 오류:', err);
     }
+    // 클린업
+    clearTimeout(beTimeout);
 
     return (
         <html
