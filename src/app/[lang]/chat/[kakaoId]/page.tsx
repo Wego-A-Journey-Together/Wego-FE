fbe9cafc4cb639e2a7674886babf650447579119
incolore9 <hansol.woff@gmail.com>
2025-04-06 19:09:56 +0900

    feat: 채팅방 리스트 페이지 이동 및 사용자 확인

--- src/app/[lang]/chat/[kakaoId]/page.tsx
+++ src/app/[lang]/chat/[kakaoId]/page.tsx
@@ -1,10 +1,26 @@
 import ChatPreview from '@/components/chat/ChatPreview';
+import getCurrentUser from '@/lib/getCurrentUser';
 import Link from 'next/link';
+import { notFound } from 'next/navigation';
 
 // 데이터 생기면 파일도 삭제
 import { chatData } from './tempData';
 
-export default function Chat() {
+type Params = { kakaoId: string };
+
+interface ChatPageProps {
+    params: Params;
+}
+
+export default async function Chat({ params }: ChatPageProps) {
+    const { kakaoId } = params;
+    const currentUser = await getCurrentUser();
+
+    // 현재 사용자와 채팅창 URL의 kakaoId가 일치하는지 확인
+    if (!currentUser || currentUser.kakaoId !== kakaoId) {
+        notFound();
+    }
+
     return (
         <>
             <div className="fixed inset-x-0 top-0 -z-10 h-full bg-[#F5F6F7]" />
@@ -16,9 +32,11 @@ export default function Chat() {
                 {/* 채팅 목록 */}
                 <div className="overflow-y-auto px-4">
                     <ul className="list-none space-y-[30px] p-0">
-                        {chatData.map((chat) => (
-                            <li key={chat.id}>
-                                <Link href={`/chat/${chat.id}`}>
+                        {chatData.map((chat, i) => (
+                            <li key={i}>
+                                <Link
+                                    href={`/chat/${kakaoId}/rooms/${chat.roomId}`}
+                                >
                                     <ChatPreview chat={chat} />
                                 </Link>
                             </li>
