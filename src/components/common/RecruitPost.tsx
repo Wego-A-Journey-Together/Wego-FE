c48fb31b3e08710d12f135f053df9e40ceea5e57
incolore9 <hansol.woff@gmail.com>
2025-04-08 17:09:04 +0900

    design: 동행 모집 카드에서 나이 연령이 스키마 값으로 보이는 부분 수정

--- src/components/common/RecruitPost.tsx
+++ src/components/common/RecruitPost.tsx
@@ -9,31 +9,39 @@ import { Badge } from '../ui/badge';
 
 //todo : 현재는 낙관적 업데이트를 위한 단순한 id 만 정의 (데이터 바인딩 x )
 
-// 나이대 표시 매핑
-const ageGroupDisplay = {
-    ALL: '전체',
-    TEENS: '10대',
-    TWENTIES: '20대',
-    THIRTIES: '30대',
-    FORTIES: '40대',
-    FIFTIES: '50대',
-    SIXTIES: '60대',
-    SEVENTIES: '70대 이상',
-};
-
-// 성별 표시 매핑
-const genderDisplay = {
-    MALE: '남자',
-    FEMALE: '여자',
-    ANY: '무관',
-};
-
 export interface PostContentProps {
     post: BEHomePost;
 }
 
 export default function RecruitPost({ post }: PostContentProps) {
     const isGroupOpen = new Date(post.closedAt) > new Date();
+
+    const ageGroupDisplay = {
+        ALL: '전체',
+        TEENS: '10대',
+        TWENTIES: '20대',
+        THIRTIES: '30대',
+        FORTIES: '40대',
+        FIFTIES: '50대',
+        SIXTIES: '60대',
+        SEVENTIES: '70대 이상',
+    };
+
+    // 성별 표시 매핑
+    const genderDisplay = {
+        MALE: '남자',
+        FEMALE: '여자',
+        ANY: '무관',
+    };
+
+    // MALE => 남성 과 같이 스키마를 UI 형식에 맞춰 변환
+    const displayedAgeGroup =
+        ageGroupDisplay[
+            post.creator.ageGroup as keyof typeof ageGroupDisplay
+        ] || post.creator.ageGroup;
+    const displayedGender =
+        genderDisplay[post.creator.gender as keyof typeof genderDisplay] ||
+        post.creator.gender;
 
     return (
         <article className="flex flex-col gap-2.5 rounded-xl bg-[#f5f6f7] px-[34px] py-[30px]">
@@ -156,12 +164,13 @@ export default function RecruitPost({ post }: PostContentProps) {
                                 <div className="h-1.5 w-px bg-gray-300" />
                                 <span className="text-xs text-[#666666]">
                                     {/*todo: 작성자 나이를 그룹으로 가져오고 있는데 이후에 숫자 업로드 구현후 보고 정하면 좋을 것 같습니다.*/}
+                                    {/* 생년월일을 n0대 나이대로 변환해서 서버로 보내는건 성공했는데, 사용자가 입력한 구체적인 생년월일 값은 스키마에 없다보니 다시 프로필 편집 창을 킬 때 서버에서 불러올 수가 없어서 일단 로컬스토리지에 구체적인 생년월일 짜게 해두었습니다. */}
                                     {/*{convertAgeRange(post.creator.age)}*/}
-                                    {post.creator.ageGroup}
+                                    {displayedAgeGroup}
                                 </span>
                                 <div className="h-1.5 w-px bg-gray-300" />
                                 <span className="text-xs text-[#666666]">
-                                    {post.creator.gender}
+                                    {displayedGender}
                                 </span>
                             </div>
                         </div>
