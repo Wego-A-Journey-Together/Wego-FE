3ad16b9749430a18e92a0c0fbe8f35d55e34c248
incolore9 <hansol.woff@gmail.com>
2025-04-08 14:06:01 +0900

    fix: PUT 메소드에도 인증 코드 추가

--- src/components/profile/ProfileEditor.tsx
+++ src/components/profile/ProfileEditor.tsx
@@ -283,6 +283,7 @@ export default function ProfileEditor({
                         gender: data.gender?.toUpperCase(),
                         ageGroup: data.ageGroup,
                     }),
+                    credentials: 'include',
                 },
             );
 
