ffd609f9413daac72bd78c6ceb73186d4926c8b6
jungdadak <junghyun21110@gmail.com>
2025-04-09 11:41:34 +0900

    refactor: 댓글 컴포넌트 세분화

---
+++ src/components/detail/CommentBundle.tsx
@@ -0,0 +1,73 @@
+import Replies from '@/components/Icons/Replies';
+import { dateFormat } from '@/lib';
+import Image from 'next/image';
+
+interface SingleCommentProps {
+    content: {
+        id: number;
+        content: string;
+        createdAt: string;
+        writer: {
+            thumbnailUrl: string;
+            nickname: string;
+        };
+        parentId: number;
+    };
+    variant: 'Comment' | 'Reply';
+}
+
+/**
+ * 댓글 or 대댓글에 따라 하나의 UI를 완성하는 컴포넌트입니다.
+ * @param content
+ * @param variant = <'Reply' | 'Comment'> 유니언 리터럴타입 입니다.
+ * @constructor
+ */
+export default function CommentBundle({
+    content,
+    variant,
+}: SingleCommentProps) {
+    return (
+        <div className="flex items-start gap-2.5">
+            {variant === 'Reply' && <Replies className={'shrink-0'} />}
+            {/*유저 정보 섹션*/}
+            <div className="flex-1 items-center gap-3">
+                {/*유저 이미지*/}
+                <div className="relative aspect-square h-8 w-8">
+                    <Image
+                        src={content.writer.thumbnailUrl}
+                        alt="유저 프로필 이미지"
+                        fill
+                        className="rounded-full object-cover"
+                    />
+                </div>
+
+                {/* 유저 정보 */}
+                <div className="w-full text-sm font-semibold text-black">
+                    {content.writer.nickname}
+                </div>
+            </div>
+
+            {/*댓글 본문 */}
+            <div className={`mt-2.5`}>
+                <p className={`text-base font-medium text-[#333333]`}>
+                    {content.content}
+                </p>
+            </div>
+
+            <div className={`mt-2.5 flex justify-start gap-[27px]`}>
+                {/*날짜 섹션*/}
+                <p className={`text-xs font-normal text-[#666666]`}>
+                    {dateFormat(content.createdAt)}
+                </p>
+                {/*todo: 답글달기 클릭시 답글 입력 (디자이너 분들께 질문하기 )*/}
+                <p
+                    className={`cursor-pointer text-xs font-medium text-[#666666]`}
+                >
+                    답글달기
+                </p>
+            </div>
+            {/*/!*세퍼레이터*!/*/}
+            {/*<div className="my-5 h-px w-full bg-[#e9e9e9]" />*/}
+        </div>
+    );
+}
--- src/components/detail/PostComment.tsx
+++ src/components/detail/PostComment.tsx
@@ -1,61 +1,32 @@
-import { dateFormat } from '@/lib';
-import Image from 'next/image';
+import CommentBundle from '@/components/detail/CommentBundle';
 
-import { postComments } from '../../../public/data/comments';
+export default async function PostComment({ postId }: { postId: number }) {
+    const NEXT_PUBLIC_NEST_BFF_URL = process.env.NEXT_PUBLIC_NEST_BFF_URL;
+    let comments = undefined;
 
-export default function PostComment() {
-    const totalComments = postComments.length;
+    try {
+        const data = await fetch(
+            // 댓글과 대댓글 합쳐서 피그마 참고해서 7로 잡았습니다.
+            `${NEXT_PUBLIC_NEST_BFF_URL}/api/gatherings/${postId}/comments?page=0&size=7`,
+            {
+                method: 'GET',
+                credentials: 'include',
+            },
+        );
+
+        comments = await data.json();
+    } catch (e) {
+        console.error('댓글 조회 실패', e);
+    }
+
     return (
         <div>
             {/*댓글 헤더*/}
             <h2 className={`mb-7.5 text-xl font-bold text-neutral-950`}>
-                댓글 {totalComments}
+                댓글 {comments?.totalElements}
             </h2>
 
-            {/*댓글 섹션*/}
-            {postComments.map((comment, idx) => (
-                <div key={`post-comment-${idx}`}>
-                    {/*유저 정보 섹션*/}
-                    <div className="flex items-center gap-3">
-                        {/*유저 이미지*/}
-                        <div className="relative aspect-square h-8 w-8">
-                            <Image
-                                src={comment.userIcon}
-                                alt="유저 프로필 이미지"
-                                fill
-                                className="rounded-full object-cover"
-                            />
-                        </div>
-
-                        {/* 유저 정보 */}
-                        <div className="w-full text-sm font-semibold text-black">
-                            {comment.userName}
-                        </div>
-                    </div>
-
-                    {/*댓글 본문 */}
-                    <div className={`mt-2.5`}>
-                        <p className={`text-base font-medium text-[#333333]`}>
-                            {comment.content}
-                        </p>
-                    </div>
-
-                    <div className={`mt-2.5 flex justify-start gap-[27px]`}>
-                        {/*날짜 섹션*/}
-                        <p className={`text-xs font-normal text-[#666666]`}>
-                            {dateFormat(comment.updatedAt)}
-                        </p>
-                        {/*todo: 답글달기 클릭시 답글 입력 (디자이너 분들께 질문하기 )*/}
-                        <p
-                            className={`cursor-pointer text-xs font-medium text-[#666666]`}
-                        >
-                            답글달기
-                        </p>
-                    </div>
-                    {/*세퍼레이터*/}
-                    <div className="my-5 h-px w-full bg-[#e9e9e9]" />
-                </div>
-            ))}
+            <CommentBundle initialComments={comments?.content} />
         </div>
     );
 }
