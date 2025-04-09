ac1151459ac61f0e0c8a47f0ea350291cad3e97a
jungdadak <junghyun21110@gmail.com>
2025-04-09 15:36:09 +0900

    feat: 댓글 컴포넌트 작성

--- src/components/detail/CommentBundle.tsx
+++ src/components/detail/CommentBundle.tsx
@@ -1,73 +1,83 @@
-import Replies from '@/components/Icons/Replies';
-import { dateFormat } from '@/lib';
-import Image from 'next/image';
+'use client';
 
-interface SingleCommentProps {
-    content: {
-        id: number;
-        content: string;
-        createdAt: string;
-        writer: {
-            thumbnailUrl: string;
-            nickname: string;
-        };
-        parentId: number;
-    };
-    variant: 'Comment' | 'Reply';
+import SingleComment from '@/components/detail/SingleComment';
+import { ChevronDown, ChevronUp } from 'lucide-react';
+import { useState } from 'react';
+
+interface Writer {
+    nickname: string;
+    thumbnailUrl: string;
+}
+
+export interface Comment {
+    id: number;
+    content: string;
+    createdAt: string;
+    writer: Writer;
+    parentId: number | null;
+    replies: Comment[];
 }
 
 /**
- * 댓글 or 대댓글에 따라 하나의 UI를 완성하는 컴포넌트입니다.
- * @param content
- * @param variant = <'Reply' | 'Comment'> 유니언 리터럴타입 입니다.
+ * 부모 댓글과 답글을 포함하는 번들 입니다.
+ * 데이터에서 부모 데이터와 자식들 데이터를 쪼개서 SingleComment 컴포넌트로 전송합니다.
+ * @param initialComments
  * @constructor
  */
-export default function CommentBundle({
-    content,
-    variant,
-}: SingleCommentProps) {
+export default function CommentBundle({ bundle }: { bundle: Comment }) {
+    const [isReplyOpen, setIsReplyOpen] = useState(false);
+
+    const replies = bundle.replies;
+
     return (
-        <div className="flex items-start gap-2.5">
-            {variant === 'Reply' && <Replies className={'shrink-0'} />}
-            {/*유저 정보 섹션*/}
-            <div className="flex-1 items-center gap-3">
-                {/*유저 이미지*/}
-                <div className="relative aspect-square h-8 w-8">
-                    <Image
-                        src={content.writer.thumbnailUrl}
-                        alt="유저 프로필 이미지"
-                        fill
-                        className="rounded-full object-cover"
-                    />
+        <div className="mb-5">
+            {/* 부모 댓글 */}
+            <SingleComment
+                content={{
+                    id: bundle.id,
+                    content: bundle.content,
+                    createdAt: bundle.createdAt,
+                    writer: bundle.writer,
+                    parentId: null,
+                }}
+                variant="Parent"
+            />
+
+            {/* 답글 목록 */}
+            {replies.length > 0 && (
+                <div>
+                    <button
+                        type={'button'}
+                        onClick={() => setIsReplyOpen((prev) => !prev)}
+                    >
+                        <p className={'text-sky-blue text-xs font-medium'}>
+                            답글 {replies.length}개
+                        </p>
+                        {isReplyOpen ? (
+                            <ChevronDown className={'h-[3.75px] w-[7.5px]'} />
+                        ) : (
+                            <ChevronUp className={'h-[3.75px] w-[7.5px]'} />
+                        )}
+                    </button>
+                    {isReplyOpen &&
+                        replies.map((reply) => (
+                            <SingleComment
+                                key={reply.id}
+                                content={{
+                                    id: reply.id,
+                                    content: reply.content,
+                                    createdAt: reply.createdAt,
+                                    writer: reply.writer,
+                                    parentId: reply.parentId || bundle.id,
+                                }}
+                                variant="Reply"
+                            />
+                        ))}
                 </div>
+            )}
 
-                {/* 유저 정보 */}
-                <div className="w-full text-sm font-semibold text-black">
-                    {content.writer.nickname}
-                </div>
-            </div>
-
-            {/*댓글 본문 */}
-            <div className={`mt-2.5`}>
-                <p className={`text-base font-medium text-[#333333]`}>
-                    {content.content}
-                </p>
-            </div>
-
-            <div className={`mt-2.5 flex justify-start gap-[27px]`}>
-                {/*날짜 섹션*/}
-                <p className={`text-xs font-normal text-[#666666]`}>
-                    {dateFormat(content.createdAt)}
-                </p>
-                {/*todo: 답글달기 클릭시 답글 입력 (디자이너 분들께 질문하기 )*/}
-                <p
-                    className={`cursor-pointer text-xs font-medium text-[#666666]`}
-                >
-                    답글달기
-                </p>
-            </div>
-            {/*/!*세퍼레이터*!/*/}
-            {/*<div className="my-5 h-px w-full bg-[#e9e9e9]" />*/}
+            {/* 세퍼레이터 */}
+            <div className="mt-5 h-px w-full bg-[#e9e9e9]" />
         </div>
     );
 }
--- src/components/detail/PostComment.tsx
+++ src/components/detail/PostComment.tsx
@@ -1,8 +1,19 @@
-import CommentBundle from '@/components/detail/CommentBundle';
+import CommentBundle, { Comment } from '@/components/detail/CommentBundle';
 
+
+
+
+
+/**
+ * 첫 페이지는 서버사이드 랜더링으로 불러와서 내려줍니다.
+ * 이후 페이지는 더보기 버튼을 클릭시마다 fetch 해서 불러오고, 마지막 페이지에서는 댓글 접기로 변경
+ * @param postId
+ * @constructor
+ */
 export default async function PostComment({ postId }: { postId: number }) {
     const NEXT_PUBLIC_NEST_BFF_URL = process.env.NEXT_PUBLIC_NEST_BFF_URL;
     let comments = undefined;
+    let initialComments: Comment[] = [];
 
     try {
         const data = await fetch(
@@ -15,6 +26,7 @@ export default async function PostComment({ postId }: { postId: number }) {
         );
 
         comments = await data.json();
+        initialComments = comments.content;
     } catch (e) {
         console.error('댓글 조회 실패', e);
     }
@@ -25,8 +37,13 @@ export default async function PostComment({ postId }: { postId: number }) {
             <h2 className={`mb-7.5 text-xl font-bold text-neutral-950`}>
                 댓글 {comments?.totalElements}
             </h2>
-
-            <CommentBundle initialComments={comments?.content} />
+            {!!comments && initialComments.length > 0 ? (
+                initialComments.map((set, setIndex) => (
+                    <CommentBundle key={setIndex} bundle={set} />
+                ))
+            ) : (
+                <div>댓글없음</div>
+            )}
         </div>
     );
 }
