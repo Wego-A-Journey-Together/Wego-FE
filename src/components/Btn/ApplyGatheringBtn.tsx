a2517c548f5e52e1f98da66ea61cee4926dcfd28
jungdadak <junghyun21110@gmail.com>
2025-04-11 00:26:01 +0900

    feat:동행 참여 공용 버튼

--- src/components/Btn/ApplyGatheringBtn.tsx
+++ src/components/Btn/ApplyGatheringBtn.tsx
@@ -1,5 +1,12 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
+import { useSession } from '@/hooks/useSession';
 import { cn } from '@/lib';
+import { fetchApply } from '@/lib/fetcher/fetchApply';
+import { useAppDispatch } from '@/redux/hooks';
+import { openLoginModal } from '@/redux/slices/modalSlice';
+import { toast } from 'sonner';
 
 interface ApplyProps {
     postId: number;
@@ -9,9 +16,33 @@ interface ApplyProps {
 /**
  * 동행 아이디를 받아서 참여 Post 요청하는 버튼
  * @param postId
+ * @param className
  */
 export default function ApplyGatheringBtn({ postId, className }: ApplyProps) {
-    const handleApplyClick = async () => {};
+    const { isAuthenticated } = useSession();
+    const dispatch = useAppDispatch();
+    const handleApplyClick = async () => {
+        // 로그인 안되면 모달띄우기
+        if (!isAuthenticated) {
+            dispatch(openLoginModal());
+            return;
+        }
+        try {
+            const message = await fetchApply(postId);
+            toast('참가 요청 결과', {
+                id: 'apply-toast',
+                description: message,
+                action: {
+                    label: '확인',
+                    onClick: () => {},
+                },
+            });
+        } catch {
+            toast.error(
+                '신청 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.',
+            );
+        }
+    };
 
     return (
         <Button
