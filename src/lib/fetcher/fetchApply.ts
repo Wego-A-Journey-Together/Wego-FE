5d9a209ac4e47579514aa0681cfa264c28c4ecd8
jungdadak <junghyun21110@gmail.com>
2025-04-11 00:10:58 +0900

    feat: 동행 참가 fetcher 함수

--- src/lib/fetcher/fetchApply.ts
+++ src/lib/fetcher/fetchApply.ts
@@ -1,8 +1,9 @@
 /**
  * 포스트 아이디를 넘기면 유저 토큰 기반으로 해당 동행에 참여 신청하는 버튼 입니다.
+ * 반환 메시지는 백엔드 필드값 사용하겠습니다.
  * @param postId
  */
-export async function fetchApply(postId: number): Promise<boolean> {
+export async function fetchApply(postId: number): Promise<string> {
     const res = await fetch(
         `${process.env.NEXT_PUBLIC_NEST_BFF_URL}/api/gatherings/apply/${postId}`,
         {
@@ -12,7 +13,9 @@ export async function fetchApply(postId: number): Promise<boolean> {
         },
     );
 
-    if (!res.ok) throw new Error('댓글 초기 로딩 실패');
+    if (!res.ok) {
+        throw new Error('동행 신청 요청 실패');
+    }
 
-    return await res.json();
+    return await res.text();
 }
